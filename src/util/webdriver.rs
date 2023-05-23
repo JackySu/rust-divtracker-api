bfdabb2715a033835a77828bfa7e4f4d21e0a532
iulx0 <hxsushi@gmail.com>
2023-05-23 21:48:47 +0800

    add webdriver args 2 ignore ssl certificate errors

--- src/util/webdriver.rs
+++ src/util/webdriver.rs
@@ -1,7 +1,17 @@
 use thirtyfour::prelude::*;
 
 pub async fn get_webdriver() -> WebDriverResult<WebDriver> {
-    let caps = DesiredCapabilities::chrome();
+    let mut caps = DesiredCapabilities::chrome();
+
+    let _ = caps.set_disable_web_security();
+    let _ = caps.add_chrome_arg("--ssl-protocol=any");
+    let _ = caps.add_chrome_arg("--ignore-ssl-errors=true");
+    let _ = caps.add_chrome_arg("--disable-extensions");
+    let _ = caps.add_chrome_arg("start-maximized");
+    let _ = caps.add_chrome_arg("window-size=1280,720");
+    let _ = caps.add_chrome_arg("disable-infobars");
+    let _ = caps.add_chrome_option("detach", true);
+
     let port = std::env::var("CHROME_PORT").unwrap_or("9515".to_string());
     let driver = WebDriver::new(format!("{}{}", "http://localhost:", port).as_str(), caps).await?;
     Ok(driver)
